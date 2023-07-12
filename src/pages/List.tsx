7d772c5fefa17201853a6096886352d4c3d13abe
jioneee <qoxx3659@gmail.com>
2023-07-12 15:35:07 +0900

    style: issue list 기본 스타일 적용

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import styled from '@emotion/styled';
 
 function List() {
   const url = 'https://api.github.com/repos/facebook/react/issues';
@@ -29,20 +30,30 @@ function List() {
       {issueList !== null ? (
         <div>
           {issueList.map((issue, index) => (
-            <tr key={index}>
-              <span>번호:#{issue.number}</span>
-              <span>제목:{issue.title}</span>
-              <span>작성자:{issue.user.id}</span>
-              <span>작성일:{new Date(issue.created_at).toLocaleDateString()}</span>
-              <span>코멘트:{issue.comments}</span>
+            <Container key={index}>
+              <TextBox>
+                {' '}
+                <Text className='bold'>#{issue.number}</Text>
+                <Text>제목:{issue.title}</Text>
+              </TextBox>
+              <TextBox>
+                {' '}
+                <Text>작성자:{issue.user.id}</Text>
+                <Text>작성일:{new Date(issue.created_at).toLocaleDateString()}</Text>
+              </TextBox>
+              <TextBox>
+                {' '}
+                <Text>코멘트:{issue.comments}</Text>
+              </TextBox>
+
               {(index + 1) % 4 === 0 && (
-                <div>
+                <TextBox className='ad'>
                   <a href='https://www.wanted.co.kr/' target='_blank' rel='noopener noreferrer'>
-                    <img src='https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100' alt='광고 이미지' />
+                    <img src='https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100' alt='wanted' />
                   </a>
-                </div>
+                </TextBox>
               )}
-            </tr>
+            </Container>
           ))}
         </div>
       ) : (
@@ -53,3 +64,31 @@ function List() {
 }
 
 export default List;
+
+const Container = styled.div`
+  box-sizing: border-box;
+  width: 700px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-bottom: solid 1px black;
+  margin: 15px auto;
+  padding: 10px;
+  .bold {
+    font-weight: 700;
+  }
+  .ad {
+    border: solid 2px blue;
+  }
+`;
+
+const TextBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 5px;
+`;
+
+const Text = styled.span`
+  box-sizing: border-box;
+  margin: auto 5px;
+`;
